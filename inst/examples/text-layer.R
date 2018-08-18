4731c427aa622f4f2dccb0ad438e43b2b5c8398a
skuethe <crazycapivara@gmail.com>
2018-08-18 19:31:00 +0200

    Add text layer example

---
+++ inst/examples/text-layer.R
@@ -0,0 +1,16 @@
+library(deckgl)
+require(geojsonio)
+
+cities <- geojsonio::canada_cities[canada_cities$capital > 0, ]
+
+lat <- cities[1, ]$lat
+lng <- cities[1, ]$long
+
+properties <- list(
+  getPosition = JS("data => [data.long, data.lat]"),
+  getText = JS("data => `${data.name} (${data.pop})`"),
+  getSize = 20
+)
+
+deckgl(latitude = lat, longitude = lng, zoom = 4) %>%
+  add_text_layer(data = cities, properties = properties)
