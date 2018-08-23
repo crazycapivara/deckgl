b8572e0cee40ce67e3cd21679b4ab0d31bf0ef9b
skuethe <crazycapivara@gmail.com>
2018-08-23 21:18:22 +0200

    Add docs to layer functions

--- R/layers_arc-layer.R
+++ R/layers_arc-layer.R
@@ -1,4 +1,8 @@
-#' Add arc layer to the deckgl widget
+#' Add an arc layer to the deckgl widget
+#'
+#' @inheritParams add_layer
+#'
+#' @seealso \url{https://deck.gl/#/documentation/deckgl-api-reference/layers/arc-layer}
 #'
 #' @export
 add_arc_layer <- function(deckgl, id = "arc-layer", data = NULL, properties = list(), ...) {
--- R/layers_hexagon-layer.R
+++ R/layers_hexagon-layer.R
@@ -10,6 +10,12 @@ properties_hexagon_layer <- list(
   getPosition = JS("data => data.COORDINATES")
 )
 
+#' Add a hexagon layer to the deckgl widget
+#'
+#' @inheritParams add_layer
+#'
+#' @seealso \url{https://deck.gl/#/documentation/deckgl-api-reference/layers/hexagon-layer}
+#'
 #' @export
 add_hexagon_layer <- function(deckgl, id = "hexagon-layer", data = NULL, properties = list(), ...) {
   # use sample data if no data is given
