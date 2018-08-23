d7d068399373b157be2103b3bd886b186f099cf0
skuethe <crazycapivara@gmail.com>
2018-08-23 21:35:26 +0200

    Add docs to 'does_it_work' func

--- R/examples.R
+++ R/examples.R
@@ -1,3 +1,7 @@
+#' Check if everything works fine
+#'
+#' @inheritParams add_mapbox_basemap
+#'
 #' @export
 does_it_work <- function(token = NULL) {
   it_works <- data.frame(lat = 37.8, lng = -122.45, text = "it works")
