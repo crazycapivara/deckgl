5ae931f8abe48a33ee78bda407767c1af4c04afa
skuethe <crazycapivara@gmail.com>
2018-08-25 22:17:50 +0200

    Add line layer example

---
+++ inst/examples/deckgl-api-example_line-layer.R
@@ -0,0 +1,13 @@
+sample_data <- "https://raw.githubusercontent.com/uber-common/deck.gl-data/master/website/bart-segments.json"
+
+properties <- list(
+  pickable = TRUE,
+  getStrokeWidth = 12,
+  getSourcePosition = JS("d => d.from.coordinates"),
+  getTargetPosition = JS("d => d.to.coordinates"),
+  getColor = JS("d => [Math.sqrt(d.inbound + d.outbound), 140, 0]"),
+  onHover = JS("({object}) => console.log(`${object.from.name} to ${object.to.name}`)")
+)
+
+deckgl(zoom = 10, pitch = 20) %>%
+  add_line_layer(data = sample_data, properties = properties)
