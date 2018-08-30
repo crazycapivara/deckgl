a944628b3da2141d54ad4bf6e3ac1c0b13a1ff47
skuethe <crazycapivara@gmail.com>
2018-08-30 18:58:44 +0200

    Add polygon-layer example

---
+++ inst/examples/deckgl-api-example-polygon-layer.R
@@ -0,0 +1,19 @@
+sample_data <- "https://raw.githubusercontent.com/uber-common/deck.gl-data/master/website/sf-zipcodes.json"
+
+properties <- list(
+  pickable = TRUE,
+  stroked = TRUE,
+  filled = TRUE,
+  wireframe = TRUE,
+  lineWidthMinPixels = 1,
+  getPolygon = JS("d => d.contour"),
+  getElevation = JS("d => d.population / d.area / 10"),
+  getFillColor= JS("d => [d.population / d.area / 60, 140, 0]"),
+  getLineColor = c(80, 80, 80),
+  getLineWidth = 1,
+  getTooltip = JS("object => `${object.zipcode}\nPopulation: ${object.population}`")
+)
+
+deckgl() %>%
+  add_layer("PolygonLayer", "polygon-layer", data = sample_data, properties = properties) %>%
+  add_mapbox_basemap()
