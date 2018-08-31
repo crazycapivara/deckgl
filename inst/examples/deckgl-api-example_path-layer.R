89b43758913d6c6951bcd8e10a448a16cde645ac
skuethe <crazycapivara@gmail.com>
2018-08-31 18:47:30 +0200

    Add path layer api example

---
+++ inst/examples/deckgl-api-example_path-layer.R
@@ -0,0 +1,16 @@
+sample_data <- "https://raw.githubusercontent.com/uber-common/deck.gl-data/master/website/bart-lines.json"
+
+properties <- list(
+  pickable = TRUE,
+  widthScale = 20,
+  widthMinPixels = 2,
+  getPath = JS("d => d.path"),
+  #getColor = c(255, 140, 20),
+  getColor = JS("d => deckglWidget.colorToRGBArray(d.color)"),
+  getWidth = 5,
+  getTooltip =JS("object => object.name")
+)
+
+deckgl() %>%
+  add_layer("PathLayer", "path-layer", data = sample_data, properties = properties) %>%
+  add_mapbox_basemap()
