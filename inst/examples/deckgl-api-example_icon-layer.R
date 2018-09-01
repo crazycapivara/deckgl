f4f6b220e536d316501504ac1c899e3785956300
skuethe <crazycapivara@gmail.com>
2018-09-01 15:20:32 +0200

    Add icon-layer api example

---
+++ inst/examples/deckgl-api-example_icon-layer.R
@@ -0,0 +1,30 @@
+sample_data <- "https://raw.githubusercontent.com/uber-common/deck.gl-data/master/website/bart-stations.json"
+
+icon_atlas <- "https://deck.gl/images/icon-atlas.png"
+
+icon_mapping <- list(
+  marker = list(
+    x = 0,
+    y = 0,
+    width = 128,
+    height = 128,
+    anchorY = 128,
+    mask = TRUE
+  )
+)
+
+properties <- list(
+  pickable = TRUE,
+  iconAtlas = icon_atlas,
+  iconMapping = icon_mapping,
+  sizeScale = 15,
+  getPosition = get_position(coordinates = "coordinates"), # JS("d => d.coordinates"),
+  getIcon = JS("d => 'marker'"),
+  getSize = 5,
+  getColor = JS("d => [Math.sqrt(d.exits), 140, 0]"),
+  getTooltip = JS ("object => `${object.name}\n${object.address}`")
+)
+
+deckgl() %>%
+  add_layer("IconLayer", "icon-layer", data = sample_data, properties = properties) %>%
+  add_mapbox_basemap()
