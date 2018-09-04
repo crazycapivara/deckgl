45393829cf994706474c40b10ecbe34ff7daa7a5
skuethe <crazycapivara@gmail.com>
2018-09-04 22:56:11 +0200

    Refactor 'parse_to_tibble'

--- data-raw/barts-stations.R
+++ data-raw/barts-stations.R
@@ -1,33 +1,8 @@
+library(magrittr)
 library(jsonlite)
-library(tibble)
 
-# get coordinates
-get_coords <- function(data) {
-  lapply(data, function(x) {
-    c(lng = x$coordinates[[1]], lat = x$coordinates[[2]])
-  }) %>% do.call(rbind, .) %>% as.tibble()
-}
+source("data-raw/parse-to-tibble.R")
 
-# get columns
-get_columns <- function(data) {
-  names_ <- names(data[[1]])
-  names_ <- names_[names_ != "coordinates"]
-  out <- lapply(names_, function(name) {
-    sapply(data, "[[", name)
-  })
-  names(out) <- names_
-  out %>% as_tibble()
-}
-
-parse_to_tibble <- function(data) {
-  coords <- get_coords(data)
-  data <- get_columns(data)
-  data$lng <- coords$lng
-  data$lat <- coords$lat
-  data
-}
-
-# parse data
 filename <- "https://raw.githubusercontent.com/uber-common/deck.gl-data/master/website/bart-stations.json"
 
 bart_stations <- read_json(filename, flatten = TRUE) %>%
@@ -40,8 +15,8 @@ bart_stations$exits %<>% as.numeric()
 deckgl() %>%
   add_text_layer(data = bart_stations,
                  getText = JS("d => d.name"),
-                 #getPosition = get_position(latitude = "lat", longitude = "lng")
-                 getPosition = get_position(coordinates = "coordinates")
+                 getPosition = get_position(latitude = "lat", longitude = "lng")
+                 #getPosition = get_position(coordinates = "coordinates")
                  )
 
 deckgl() %>%
