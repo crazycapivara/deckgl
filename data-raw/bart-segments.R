ba6871687001c050e31818e3b66996876310a070
skuethe <crazycapivara@gmail.com>
2018-09-04 23:36:23 +0200

    Fix styler warnings

--- data-raw/bart-segments.R
+++ data-raw/bart-segments.R
@@ -11,10 +11,11 @@ to <- lapply(x, "[[", "to")
 from_coords <- get_coords(from)
 from_cols <- get_columns(from)
 
-to_coords <-  get_coords(to)
+to_coords <- get_coords(to)
 to_cols <- get_columns(to)
 
-bart_segments <- tibble(from_name = from_cols$name, from_lat = from_coords$lat, from_lng = from_coords$lng,
-                        to_name = to_cols$name, to_lat = to_coords$lat, to_lng = to_coords$lng,
-                        inbound = sapply(x, "[[", "inbound"), outbound = sapply(x, "[[", "outbound")
+bart_segments <- tibble(
+  from_name = from_cols$name, from_lat = from_coords$lat, from_lng = from_coords$lng,
+  to_name = to_cols$name, to_lat = to_coords$lat, to_lng = to_coords$lng,
+  inbound = sapply(x, "[[", "inbound"), outbound = sapply(x, "[[", "outbound")
 )
--- data-raw/sf-bike-parking.R
+++ data-raw/sf-bike-parking.R
@@ -2,7 +2,7 @@ library(jsonlite)
 
 source("data-raw/parse-to-tibble.R")
 
-filename = "https://raw.githubusercontent.com/uber-common/deck.gl-data/master/website/sf-bike-parking.json"
+filename <- "https://raw.githubusercontent.com/uber-common/deck.gl-data/master/website/sf-bike-parking.json"
 
 sf_bike_parking <- read_json(filename) %>%
   parse_to_tibble(coordinates = "COORDINATES")
@@ -11,10 +11,12 @@ names(sf_bike_parking) %<>% tolower()
 
 # test data
 deckgl() %>%
-  add_grid_layer(data = sf_bike_parking,
-                 getPosition = get_position(latitude = "lat", longitude = "lng"),
-                 pickable = TRUE,
-                 extruded = TRUE,
-                 cellSize = 200,
-                 elevationScale = 4,
-                 getTooltip = JS("object => `${object.count}`"))
+  add_grid_layer(
+    data = sf_bike_parking,
+    getPosition = get_position(latitude = "lat", longitude = "lng"),
+    pickable = TRUE,
+    extruded = TRUE,
+    cellSize = 200,
+    elevationScale = 4,
+    getTooltip = JS("object => `${object.count}`")
+  )
