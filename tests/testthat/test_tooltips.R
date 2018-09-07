e62543aa0af6e77a73cba9631f1cf1c667ac5abc
skuethe <crazycapivara@gmail.com>
2018-09-07 14:54:02 +0200

    Add another test for 'pickable' property

--- tests/testthat/test_tooltips.R
+++ tests/testthat/test_tooltips.R
@@ -4,7 +4,7 @@ properties_ <- function(deckgl) {
   deckgl$x$layers[[1]]$properties
 }
 
-test_that("pickable", {
+test_that("pickable not set", {
   # Prepare
   sample_data <- "some-sample-data"
   properties <- list(
@@ -17,4 +17,20 @@ test_that("pickable", {
 
   # Assert
   expect_equal(properties_(deckgl)$pickable, TRUE)
+})
+
+test_that("pickable set to FALSE", {
+  # Prepare
+  sample_data <- "some-sample-data"
+  properties <- list(
+    pickable = FALSE,
+    getTooltip = JS("object => `count: ${object.points.length}`")
+  )
+
+  # Act
+  deckgl <- deckgl() %>%
+    add_hexagon_layer(data = sample_data, properties = properties)
+
+  # Assert
+  expect_equal(properties_(deckgl)$pickable, FALSE)
 })
