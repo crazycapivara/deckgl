356ace43b000ae4fe0eb28500ba8a53e2e65282d
skuethe <crazycapivara@gmail.com>
2018-09-07 21:00:05 +0200

    Fix styler warnings

--- R/layers_icon-layer.R
+++ R/layers_icon-layer.R
@@ -15,13 +15,13 @@ add_icon_layer <- function(deckgl, id = "icon-layer", data = NULL, properties = 
 # TODO: Maybe export function as 'default_icon_properties'
 properties_icon <- function(properties = list(), ...) {
   if (c(properties, list(...))$iconAtlas %>% is.null()) {
-    properties$defaultIcon = TRUE
-    properties$iconAtlas = encode_icon_atlas()
-    properties$iconMapping = list(marker = icon_definition())
-    properties$getIcon = JS("d => 'marker'")
-    properties$getColor = c(240, 140, 0)
-    properties$sizeScale = 15
-    properties$getSize = 5
+    properties$defaultIcon <- TRUE
+    properties$iconAtlas <- encode_icon_atlas()
+    properties$iconMapping <- list(marker = icon_definition())
+    properties$getIcon <- JS("d => 'marker'")
+    properties$getColor <- c(240, 140, 0)
+    properties$sizeScale <- 15
+    properties$getSize <- 5
   }
 
   properties
