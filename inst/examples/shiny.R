3c36cd4afe2340aab0d87cbc2966a2acde0652d0
skuethe <crazycapivara@gmail.com>
2018-09-07 14:05:03 +0200

    Add shiny example

---
+++ inst/examples/shiny.R
@@ -0,0 +1,20 @@
+library(shiny)
+library(deckgl)
+
+view <- fluidPage(
+  h1("deckgl for R"),
+  deckglOutput("deckgl"),
+  style = "font-family: ubuntu;"
+)
+
+backend <- function(input, output) {
+  output$deckgl <- renderDeckgl({
+    deckgl(pitch = 45) %>% add_hexagon_layer(
+      pickable = TRUE,
+      getTooltip = JS("object => `count: ${object.points.length}`")
+    ) %>%
+      add_mapbox_basemap()
+  })
+}
+
+shinyApp(view, backend)
