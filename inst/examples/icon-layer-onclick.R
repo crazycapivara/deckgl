ed8614f72f45b3ef8882ab144b7873544f99da0f
skuethe <crazycapivara@gmail.com>
2018-09-08 19:47:01 +0200

    Update examples

--- inst/examples/icon-layer-onclick.R
+++ inst/examples/icon-layer-onclick.R
@@ -5,6 +5,7 @@ deckgl() %>%
   add_icon_layer(
     data = sample_data,
     getPosition = get_position(coordinates = "coordinates"),
+    getColor = JS("d => [Math.sqrt(d.exits), 140, 0]"), # overwrite default color
     pickable = TRUE,
     onClick = JS("info => console.log(info)")
   )
