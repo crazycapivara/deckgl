5a46d9ad7e52473c947e770fd1340affd82caa18
skuethe <crazycapivara@gmail.com>
2018-09-11 19:25:07 +0200

    Store raw json data from deckgl api examples

---
+++ data-raw/json/get-data.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+wget https://raw.githubusercontent.com/uber-common/deck.gl-data/master/website/bart-segments.json
+wget https://raw.githubusercontent.com/uber-common/deck.gl-data/master/website/bart-stations.json
+wget https://raw.githubusercontent.com/uber-common/deck.gl-data/master/website/sf-bike-parking.json
