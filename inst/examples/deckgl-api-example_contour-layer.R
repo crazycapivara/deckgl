62145250cfec288c8ac04bd3c1267a7e51604182
skuethe <crazycapivara@gmail.com>
2018-09-13 22:05:26 +0200

    Update contour-example

--- inst/examples/deckgl-api-example_contour-layer.R
+++ inst/examples/deckgl-api-example_contour-layer.R
@@ -1,9 +1,21 @@
 sample_data <- "https://raw.githubusercontent.com/uber-common/deck.gl-data/master/website/sf-bike-parking.json"
 
 contours = list(
-  list(threshold = 1, color = c(255, 0, 0), strokeWidth = 2),
-  list(threshold = 5, color = c(0, 255, 0), strokeWidth = 3),
-  list(threshold = 15, color = c(0, 0, 255), strokeWidth = 4)
+  contour_definition(
+    threshold = 1,
+    color = c(255, 0, 0),
+    strokeWidth = 1
+  ),
+  contour_definition(
+    threshold = 5,
+    color = c(0, 255, 0),
+    strokeWidth = 3
+  ),
+  contour_definition(
+    threshold = 15,
+    color = c(0, 0, 255),
+    strokeWidth = 4
+  )
 )
 
 contour_properties <- list(
