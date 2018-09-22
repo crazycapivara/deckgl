3e0c04a8cbf8f9ce0d084b0eb17e7437b84f8774
skuethe <crazycapivara@gmail.com>
2018-09-22 19:14:11 +0200

    Add function to create a data accessor and update 'get_position' r-docs

--- R/properties.R
+++ R/properties.R
@@ -1,11 +1,13 @@
-#' Create a getPosition property
+#' Create a getPosition data accessor
+#'
+#' Method called to retrieve the position of each object.
 #'
 #' @param latitude latitude key in data object
 #' @param longitude longitude key in data object
 #' @param coordinates coordinates key in data object
 #'   (in this case \code{latitude} and \code{longitude} parameters are ignored)
 #'
-#' @return getPosition property
+#' @return JavaScript code evaluated on the client-side
 #'
 #' @export
 get_position <- function(latitude = NULL, longitude = NULL, coordinates = NULL) {
@@ -15,4 +17,17 @@ get_position <- function(latitude = NULL, longitude = NULL, coordinates = NULL) 
       sprintf("data.%s", coordinates)
     )
   sprintf("data => %s", coordinates_) %>% JS()
+}
+
+#' Create a data accessor
+#'
+#' Method called to retrieve the value of the given property of each object.
+#'
+#' @param property property of data object
+#'
+#' @return JavaScript code evaluated on the client-side
+#'
+#' @export
+get_value <- function(property) {
+  sprintf("data => data.%s", property) %>% JS()
 }
