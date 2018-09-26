49e35fbcf0ab65404825dd9433d750b1a1f87c15
skuethe <crazycapivara@gmail.com>
2018-09-26 21:45:56 +0200

    Add icon-layer example to markdown docs

---
+++ inst/examples/deckgl-api-reference/icon-layer.R
@@ -0,0 +1,22 @@
+## @knitr icon-layer
+sample_data <- paste0(
+  "https://raw.githubusercontent.com/",
+  "uber-common/deck.gl-data/",
+  "master/website/bart-stations.json"
+)
+
+properties <- list(
+  pickable = TRUE,
+  iconAtlas = encode_icon_atlas(),
+  iconMapping = list(marker = icon_definition()),
+  sizeScale = 10,
+  getPosition = get_property("coordinates"),
+  getIcon = JS("d => 'marker'"),
+  getSize = 5,
+  getColor = JS("d => [Math.sqrt(d.exits), 140, 0]"),
+  getTooltip = JS("object => `${object.name}<br/>${object.address}`")
+)
+
+deckgl(zoom = 10, pitch = 45) %>%
+  add_icon_layer(data = sample_data, properties = properties) %>%
+  add_mapbox_basemap()
