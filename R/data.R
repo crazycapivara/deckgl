2e1d6a8864bd94248c08837640e14e6252eeeb7f
skuethe <crazycapivara@gmail.com>
2018-10-03 22:04:41 +0200

    Add sf-bike-parking data set and update data descriptions

--- R/data.R
+++ R/data.R
@@ -2,14 +2,14 @@
 #'
 #' @format tibble with 45 rows and 8 variables:
 #' \describe{
-#'   \item{inbound}{inbound trips}
-#'   \item{outbound}{outbound trips}
-#'   \item{from_name}{name of source station}
-#'   \item{from_lng}{longitude of source station}
-#'   \item{from_lat}{latitude of source station}
-#'   \item{to_name}{name of target station}
-#'   \item{to_lng}{longitude of target station}
-#'   \item{to_lat}{latitude of target station}
+#'   \item{inbound}{number of inbound trips}
+#'   \item{outbound}{number of outbound trips}
+#'   \item{from_name}{source station name}
+#'   \item{from_lng}{source station longitude}
+#'   \item{from_lat}{source station latitude}
+#'   \item{to_name}{target station name}
+#'   \item{to_lng}{target station longitude}
+#'   \item{to_lat}{target station latitude}
 #' }
 #'
 #' @source \url{"https://raw.githubusercontent.com/uber-common/deck.gl-data/master/website/bart-segments.json"}
@@ -22,11 +22,18 @@
 #'   \item{name}{station name}
 #'   \item{code}{two-letter station code}
 #'   \item{address}{station address}
-#'   \item{entries}{entries at station}
-#'   \item{exits}{exits at station}
+#'   \item{entries}{number of entries}
+#'   \item{exits}{number of exits}
 #'   \item{lng}{station longitude}
 #'   \item{lat}{station latitude}
 #' }
 #'
 #' @source \url{"https://raw.githubusercontent.com/uber-common/deck.gl-data/master/website/bart-stations.json"}
 "bart_stations"
+
+#' sf bike parking
+#'
+#' @format tibble with 2520 rows and 5 variables
+#'
+#' @source \url{https://raw.githubusercontent.com/uber-common/deck.gl-data/master/website/sf-bike-parking.json}
+"sf_bike_parking"
