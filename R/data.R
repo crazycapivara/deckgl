51a2e4d720de1c6b17360282a4cc55281ba996b7
skuethe <crazycapivara@gmail.com>
2018-10-03 19:10:21 +0200

    Add bart-segments data set

---
+++ R/data.R
@@ -0,0 +1,16 @@
+#' bart segments
+#'
+#' @format tibble with 45 rows and 8 variables:
+#' \describe{
+#'   \item{inbound}{inbound trips}
+#'   \item{outbound}{outbound trips}
+#'   \item{from_name}{name of source station}
+#'   \item{from_lng}{longitude of source station}
+#'   \item{from_lat}{latitude of source station}
+#'   \item{to_name}{name of target station}
+#'   \item{to_lng}{longitude of target station}
+#'   \item{to_lat}{latitude of target station}
+#' }
+#'
+#' @source \url{"https://raw.githubusercontent.com/uber-common/deck.gl-data/master/website/bart-segments.json"}
+"bart_segments"
