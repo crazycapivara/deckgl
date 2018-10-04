82a4fb5a64b500714509f918facb6880920b59ae
skuethe <crazycapivara@gmail.com>
2018-10-04 18:58:02 +0200

    Update data set descriptions

--- R/data.R
+++ R/data.R
@@ -1,15 +1,18 @@
+#' @importFrom tibble tibble
+NULL
+
 #' bart segments
 #'
 #' @format tibble with 45 rows and 8 variables:
 #' \describe{
 #'   \item{inbound}{number of inbound trips}
 #'   \item{outbound}{number of outbound trips}
-#'   \item{from_name}{source station name}
-#'   \item{from_lng}{source station longitude}
-#'   \item{from_lat}{source station latitude}
-#'   \item{to_name}{target station name}
-#'   \item{to_lng}{target station longitude}
-#'   \item{to_lat}{target station latitude}
+#'   \item{from_name}{name of source station }
+#'   \item{from_lng}{longitude of source station}
+#'   \item{from_lat}{latitude of source station}
+#'   \item{to_name}{name of target station}
+#'   \item{to_lng}{longitude of target station}
+#'   \item{to_lat}{latitude of target station}
 #' }
 #'
 #' @source \url{"https://raw.githubusercontent.com/uber-common/deck.gl-data/master/website/bart-segments.json"}
@@ -21,11 +24,11 @@
 #' \describe{
 #'   \item{name}{station name}
 #'   \item{code}{two-letter station code}
-#'   \item{address}{station address}
+#'   \item{address}{address}
 #'   \item{entries}{number of entries}
 #'   \item{exits}{number of exits}
-#'   \item{lng}{station longitude}
-#'   \item{lat}{station latitude}
+#'   \item{lng}{longitude}
+#'   \item{lat}{latitude}
 #' }
 #'
 #' @source \url{"https://raw.githubusercontent.com/uber-common/deck.gl-data/master/website/bart-stations.json"}
@@ -33,7 +36,14 @@
 
 #' sf bike parking
 #'
-#' @format tibble with 2520 rows and 5 variables
+#' @format tibble with 2520 rows and 5 variables:
+#' \describe{
+#'   \item{address}{address}
+#'   \item{racks}{number of racks}
+#'   \item{spaces}{number of spaces}
+#'   \item{lng}{longitude}
+#'   \item{lat}{latidude}
+#' }
 #'
 #' @source \url{https://raw.githubusercontent.com/uber-common/deck.gl-data/master/website/sf-bike-parking.json}
 "sf_bike_parking"
