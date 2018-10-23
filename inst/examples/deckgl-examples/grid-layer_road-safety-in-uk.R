5feab4c420179584cd0cbb4a3db384b9f8cf194a
skuethe <crazycapivara@gmail.com>
2018-10-23 22:36:05 +0200

    Refactor road-safety-in-uk-example

--- inst/examples/deckgl-examples/grid-layer_road-safety-in-uk.R
+++ inst/examples/deckgl-examples/grid-layer_road-safety-in-uk.R
@@ -1,19 +1,16 @@
-library(deckgl)
+## @knitr road-safety-in-uk
+library("deckgl")
+library("data.table")
 
-sample_data <- paste0(
+data_url <- paste0(
   "https://raw.githubusercontent.com/",
   "uber-common/deck.gl-data/",
   "master/examples/3d-heatmap/heatmap-data.csv"
 )
-
-df <- data.table::fread(sample_data)
-
+sample_data <- fread(data_url)
 color_to_rgb <- function(color) col2rgb(color) %>% as.vector()
-
-color_range <- lapply(
-  c("#0198BD", "#49E3CE", "#D8FEB5", "#FEEDB1", "#FEAD54", "#D1374E"),
-  color_to_rgb
-)
+color_range <- c("#0198BD", "#49E3CE", "#D8FEB5", "#FEEDB1", "#FEAD54", "#D1374E") %>%
+  lapply(color_to_rgb)
 
 light_settings <- list(
   lightsPosition = c(-0.144528, 49.739968, 8000, -3.807751, 54.104682, 8000),
@@ -24,15 +21,20 @@ light_settings <- list(
   numberOfLights = 2
 )
 
-deckgl(
+initial_view_state <- list(
   longitude = -1.4157267858730052,
   latitude = 52.232395363869415,
   zoom = 6.6,
   pitch = 40.5,
   bearing = -27.396674584323023
+)
+
+deckgl(
+  initialViewState = initial_view_state,
+  style = list(background = "black")
 ) %>%
   add_grid_layer(
-    data = df,
+    data = sample_data,
     getPosition = get_position("lat", "lng"),
     cellSize = 2000,
     elevationScale = 50,
@@ -41,7 +43,4 @@ deckgl(
     colorRange = color_range,
     lightSettings = light_settings,
     getTooltip = JS("object => `${object.count} accidents`")
-  ) %>%
-  onRender("() => deckglWidget.element.style.background = 'black'")
-
-# %>% add_mapbox_basemap(style = "mapbox://styles/mapbox/dark-v9")
+  )
