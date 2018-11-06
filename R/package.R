b1f6a1d065e869258650842befc4b75029d269a3
skuethe <crazycapivara@gmail.com>
2018-11-06 21:25:05 +0100

    Fix cran warning

--- R/package.R
+++ R/package.R
@@ -12,5 +12,4 @@ run_api_example <- function(script_name = "arc-layer.R") {
   system.file("examples/deckgl-api-reference", package = "deckgl") %>%
     paste0("/", script_name) %>%
     source()
-  deck
 }
