a0a08bd72f066b79e16a1a6de2df9bdd250fa95b
skuethe <crazycapivara@gmail.com>
2018-11-06 18:55:38 +0100

    Refactor

--- R/zzz.R
+++ R/zzz.R
@@ -1,13 +1,11 @@
 .onAttach <- function(libname, pkgname) { # nocov start
-  mapbox_message <- c(
-    "\n",
-    "If you want to add a base map from mapbox, ",
-    "it is recommended that you store your api token in an environment variable called MAPBOX_API_TOKEN.\n"
-  )
-  if (!identical(Sys.getenv("MAPBOX_API_TOKEN"), "")) mapbox_message <- ""
-
-  #filename <- system.file("htmlwidgets/deckgl.yaml", package = "deckgl")
-  #deckgljs_version <- yaml::yaml.load_file(filename)$dependencies[[1]]$version
+  mapbox_message <- ""
+  if (Sys.getenv("MAPBOX_API_TOKEN") == "") {
+    mapbox_message <- c(
+      "\nIf you want to add a base map from mapbox, ",
+      "it is recommended that you store your api token in an environment variable called MAPBOX_API_TOKEN.\n"
+    )
+  }
 
   packageStartupMessage(
     pkgname, " ", getNamespaceVersion(pkgname), " wrapping deckgljs ", deckgljs_version(), "\n",
