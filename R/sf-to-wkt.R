1b5b276ac58773d2885f36fa0eee683467ca7a11
skuethe <crazycapivara@gmail.com>
2018-12-15 21:23:31 +0100

    Refactor 'wellknown'

--- R/sf-to-wkt.R
+++ R/sf-to-wkt.R
@@ -1,12 +1,44 @@
+#' @name wellknown
 #' @export
 wellknown <- function(data) {
-  geometry_class <- st_geometry(data) %>% class()
-  if ("sfc_MULTIPOLYGON" %in% geometry_class){
-    suppressWarnings(data <- sf::st_cast(data, "POLYGON"))
-  }
+  UseMethod("wellknown", sf::st_geometry(data))
+}
+
+#' @name wellknown
+#' @export
+wellknown.sfc_MULTIPOLYGON <- function(data) {
+  suppressWarnings(sf::st_cast(data, "POLYGON")) %>%
+    wellknown()
+}
+
+#' @name wellknown
+#' @export
+wellknown.sfc_MULTILINESTRING <- function(data) {
+  suppressWarnings(sf::st_cast(data, "LINESTRING")) %>%
+    wellknown()
+}
+
+#' @name wellknown
+#' @export
+wellknown.default <- function(data) {
+  data$geom_wkt <- sf::st_geometry(data) %>%
+    sf::st_as_text()
+  sf::st_geometry(data) <- NULL
+  data
+}
+
+wellknown_ <- function(data) {
+  geometry_class <- sf::st_geometry(data) %>% class()
+  #if ("sfc_MULTIPOLYGON" %in% geometry_class) {
+  #  suppressWarnings(data <- sf::st_cast(data, "POLYGON"))
+  #}
+
+  #if ("sfc_MULTILINESTRING" %in% geometry_class) {
+  #  suppressWarnings(data <- sf::st_cast(data, "LINESTRING"))
+  #}
 
   data$geom_wkt <- sf::st_geometry(data) %>%
-    st_as_text()
+    sf::st_as_text()
   sf::st_geometry(data) <- NULL
   data
 }
