57c85724c52b6ef1c88bd0f6813d2699652bc18a
skuethe <crazycapivara@gmail.com>
2018-12-16 18:53:20 +0100

    Refactor 'wellknown'

--- R/sf-to-wkt.R
+++ R/sf-to-wkt.R
@@ -1,3 +1,12 @@
+cast_this <- function(data, to) {
+  suppressWarnings(sf::st_cast(data, to)) %>%
+    wellknown()
+}
+
+#' Parse geometry column of \code{sf} object to wellknown text
+#'
+#' @param data object of class \code{sf}
+#'
 #' @name wellknown
 #' @export
 wellknown <- function(data) {
@@ -7,15 +16,23 @@ wellknown <- function(data) {
 #' @name wellknown
 #' @export
 wellknown.sfc_MULTIPOLYGON <- function(data) {
-  suppressWarnings(sf::st_cast(data, "POLYGON")) %>%
-    wellknown()
+  cast_this(data, "POLYGON")
+  # suppressWarnings(sf::st_cast(data, "POLYGON")) %>%
+  #   wellknown()
 }
 
 #' @name wellknown
 #' @export
 wellknown.sfc_MULTILINESTRING <- function(data) {
-  suppressWarnings(sf::st_cast(data, "LINESTRING")) %>%
-    wellknown()
+  cast_this(data, "LINESTRING")
+  # suppressWarnings(sf::st_cast(data, "LINESTRING")) %>%
+  #   wellknown()
+}
+
+#' @name wellknown
+#' @export
+wellknown.sfc_MULTIPOINT <- function(data) {
+  cast_this(data, "POINT")
 }
 
 #' @name wellknown
@@ -27,6 +44,7 @@ wellknown.default <- function(data) {
   data
 }
 
+# obsolete
 wellknown_ <- function(data) {
   geometry_class <- sf::st_geometry(data) %>% class()
   #if ("sfc_MULTIPOLYGON" %in% geometry_class) {
