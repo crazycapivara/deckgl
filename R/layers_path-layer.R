0d6f7da15451ff9d7e42e390473508aedda5dade
skuethe <crazycapivara@gmail.com>
2018-12-16 21:57:26 +0100

    Add S3 methods for 'add_path_layer'

--- R/layers_path-layer.R
+++ R/layers_path-layer.R
@@ -9,6 +9,18 @@
 #' @example inst/examples/deckgl-api-reference/path-layer.R
 #'
 #' @export
-add_path_layer <- function(deckgl, id = "path-layer", data = NULL, properties = list(), ...) {
+add_path_layer <- function(deckgl, id, data, properties, ...) {
+  UseMethod("add_path_layer", data)
+}
+
+#' @name add_path_layer
+#' @export
+add_path_layer.default <- function(deckgl, id = "path-layer", data = NULL, properties = list(), ...) {
   add_layer(deckgl, "PathLayer", id, data, properties, ...)
+}
+
+#' @name add_path_layer
+#' @export
+add_path_layer.sf <- function(deckgl, id = "path-layer", data = NULL, properties = list(), ...) {
+  add_path_layer(deckgl, id, wellknown(data), properties, ..., getPath = get_wellknown())
 }
