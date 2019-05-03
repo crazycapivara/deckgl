38c83cf2263dff5bc6b09936fcbac6805f786c31
skuethe <crazycapivara@gmail.com>
2019-05-03 15:50:03 +0200

    Update docs

--- inst/examples/deckgl-api-reference/bitmap-layer.R
+++ inst/examples/deckgl-api-reference/bitmap-layer.R
@@ -1,4 +1,8 @@
-image <- "https://raw.githubusercontent.com/uber-common/deck.gl-data/master/website/sf-districts.png"
+image <- paste0(
+  "https://raw.githubusercontent.com/",
+  "uber-common/deck.gl-data/master/",
+  "website/sf-districts.png"
+)
 bounds <- c(-122.5190, 37.7045, -122.355, 37.829)
 
 deck <- deckgl() %>%
