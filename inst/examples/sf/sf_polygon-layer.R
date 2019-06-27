5066c393600a760684ce41ca61bfbd6169e5a4ce
skuethe <crazycapivara@gmail.com>
2019-06-27 14:59:36 +0200

    Refactor example

--- inst/examples/sf/sf_polygon-layer.R
+++ inst/examples/sf/sf_polygon-layer.R
@@ -1,18 +1,17 @@
 library(deckgl)
 library(sf)
 
-nc <- system.file("shape/nc.shp", package = "sf") %>%
+sample_data <- system.file("shape/nc.shp", package = "sf") %>%
   st_read(quiet = TRUE)
 
-nc <- st_cast(nc, "POLYGON")
-nc4326 <- st_transform(nc, 4326)
+# sample_data <- spData::hawaii %>% st_transform(4326)
 
-center <- st_bbox(nc) %>% st_as_sfc() %>% st_centroid() %>% st_coordinates() %>% as.vector()
+center <- st_bbox(sample_data) %>% st_as_sfc() %>% st_centroid() %>% st_coordinates() %>% as.vector()
 
 deckgl(latitude = center[2], longitude = center[1], zoom = 6) %>%
   add_polygon_layer(
     filled = TRUE,
-    data = nc4326,
+    data = sample_data,
     getPolygon = ~geometry,
     lineWidthMinPixels = 1,
     getLineColor = c(250, 80, 80),
