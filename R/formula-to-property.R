ddc96336c5867cc96afcec265a2504419d10fc98
skuethe <crazycapivara@gmail.com>
2019-06-28 08:49:09 +0200

    Refactor

--- R/formula-to-property.R
+++ R/formula-to-property.R
@@ -2,7 +2,7 @@ formula_to_property <- function(properties) {
   for (key in names(properties)) {
     value <- properties[[key]]
     if (inherits(value, "formula")) {
-      x <- labels(stats::terms(value))
+      x <- all.vars(value)
       if (length(x) > 1) {
         properties[[key]] <- sprintf("data => [%s]", paste0("data.", x, collapse = ", ")) %>%
           JS()
