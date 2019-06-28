96689f04204f3f143e44340e179520adb3bf4464
skuethe <crazycapivara@gmail.com>
2019-06-28 19:07:32 +0200

    Update screen-grid-layer example

--- inst/examples/deckgl-api-reference/screen-grid-layer.R
+++ inst/examples/deckgl-api-reference/screen-grid-layer.R
@@ -10,16 +10,26 @@ color_range <- c("#0198BD", "#49E3CE", "#D8FEB5", "#FEEDB1", "#FEAD54", "#D1374E
   lapply(color_to_rgb)
 
 properties <- list(
-  pickable = FALSE,
   opacity = 0.8,
   cellSizePixels = 50,
   colorRange = color_range,
-  getPosition = get_property("COORDINATES"),
-  getWeight = get_property("SPACES")
+  getPosition = ~COORDINATES,
+  getWeight = ~SPACES
 )
 
 deck <- deckgl() %>%
   add_screen_grid_layer(data = sample_data, properties = properties) %>%
+  add_mapbox_basemap()
+
+# Use a data object instead of the data url above
+deck <- deckgl() %>%
+  add_screen_grid_layer(
+    data = sf_bike_parking,
+    properties = properties,
+    # overwrite properties
+    getPosition = ~lng + lat,
+    getWeight = ~spaces
+  ) %>%
   add_mapbox_basemap()
 
 if (interactive()) deck
