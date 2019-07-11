098d90567630537fb3cf3c901fa2050d13818fb0
skuethe <crazycapivara@gmail.com>
2019-07-11 21:46:16 +0200

    Refactor

--- javascript/src/deckgl-widget.js
+++ javascript/src/deckgl-widget.js
@@ -6,34 +6,36 @@ global._deckGLWidget = {};
 export default function(el, width, height) {
   console.log("I am your DeckGLWidget!");
 
+  self = this;
+
   var deckGL = null;
 
-  this.renderValue = function(x) {
-    logVersions();
+  self.renderValue = function(x) {
+    self._logVersions();
     console.log("el", el, "x", x);
     _deckGLWidget.element = el;
-    createTooltipElement(el);
+    self._createTooltipElement();
     deckGL = _deckGLWidget.deckGL = makeDeck(el, x);
     var layers = _deckGLWidget.layers = makeLayers(x.layers);
     deckGL.setProps({ layers: layers });
   };
 
-  this.resize = function(width, height) { };
+  self.resize = function(width, height) { };
 
-  this.getDeckGL = function() {
+  self.getDeckGL = function() {
     return deckGL;
   };
+
+  self._logVersions = function() {
+    console.log("deck.gl version: " + deck.version);
+    if (typeof mapboxgl !== "undefined") {
+      console.log("mapbox-gl version: " + mapboxgl.version);
+    }
+  };
+
+  self._createTooltipElement = function() {
+    var tooltipElement = _deckGLWidget.tooltipElement = document.createElement("div");
+    tooltipElement.id = "tooltip";
+    el.appendChild(tooltipElement);
+  };
 }
-
-var logVersions = function() {
-  console.log("deck.gl version: " + deck.version);
-  if (typeof mapboxgl !== "undefined") {
-    console.log("mapbox-gl version: " + mapboxgl.version);
-  }
-};
-
-var createTooltipElement = function(el) {
-  var tooltipElement = _deckGLWidget.tooltipElement = document.createElement("div");
-  tooltipElement.id = "tooltip";
-  el.appendChild(tooltipElement);
-};
