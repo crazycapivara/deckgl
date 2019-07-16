ceeaa89db8589822a4230d9e9aa5105625b6f101
skuethe <crazycapivara@gmail.com>
2019-07-16 13:04:32 +0200

    Remove dead code

--- javascript/src/index.js
+++ javascript/src/index.js
@@ -1,5 +1,14 @@
-import R2DeckGL from "./r2deckgl";
+import DeckGLWidget from "./deckgl-widget";
 
-var r2DeckGL = new R2DeckGL();
-var el = document.getElementById("widget");
-el.innerHTML = r2DeckGL.renderValue();
+HTMLWidgets.widget({
+  name: "deckgl",
+  type: "output",
+  factory: function(el, width, height) {
+    var deckGLWidget = new DeckGLWidget(el, width, height);
+    return {
+      renderValue: deckGLWidget.renderValue,
+      resize: deckGLWidget.resize,
+      getDeckGL: deckGLWidget.getDeckGL
+    };
+  }
+});
