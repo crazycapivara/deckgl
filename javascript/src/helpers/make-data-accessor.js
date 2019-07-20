0fb274aa5ec3efa2066f6d93d772a763e276be46
skuethe <crazycapivara@gmail.com>
2019-07-20 20:11:30 +0200

    Refactor

--- javascript/src/helpers/make-data-accessor.js
+++ javascript/src/helpers/make-data-accessor.js
@@ -4,9 +4,13 @@ export default function(propertyName, keys) {
   console.log(propertyName, "make data accessor");
   if (typeof keys === "string") {
     const key = keys;
-    // return data => data[key];
-    return data => (propertyName.includes("Color") && typeof data[key] === "string") ?
-      hexColorToRGBArray(data[key]) : data[key];
+    const isColor = propertyName.includes("Color");
+    if (isColor) {
+      // console.log("is color");
+      return data => typeof data[key] === "string" ? hexColorToRGBArray(data[key]) : data[key];
+    }
+
+    return data => data[key];
   }
 
   return data => keys.map(key => data[key]);
