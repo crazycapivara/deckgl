d96a79db18dfd9ea15a4f669b5e1ba9df643e370
skuethe <crazycapivara@gmail.com>
2019-07-21 23:06:01 +0200

    Update startup message

--- R/zzz.R
+++ R/zzz.R
@@ -3,12 +3,12 @@
   if (Sys.getenv("MAPBOX_API_TOKEN") == "") {
     mapbox_message <- c(
       "\nIf you want to add a base map from mapbox, ",
-      "it is recommended that you store your api token in an environment variable called MAPBOX_API_TOKEN.\n"
+      "it is recommended that you store your API token in an environment variable called MAPBOX_API_TOKEN.\n"
     )
   }
 
   packageStartupMessage(
-    pkgname, " ", getNamespaceVersion(pkgname), " wrapping deckgljs ", deckgljs_version(), "\n",
+    pkgname, " ", getNamespaceVersion(pkgname), " wrapping deckgl-js ", deckgljs_version(), "\n",
     "  Documentation: https://crazycapivara.github.io/deckgl/\n",
     "  Issues, notes and bleeding edge: https://github.com/crazycapivara/deckgl\n",
     mapbox_message
