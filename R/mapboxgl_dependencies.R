154ecff56802f09d05dea351335540fe8fdd54ac
skuethe <crazycapivara@gmail.com>
2019-07-21 20:07:28 +0200

    Fix version number

--- R/mapboxgl_dependencies.R
+++ R/mapboxgl_dependencies.R
@@ -2,7 +2,7 @@ mapboxgl_dependencies <- function() {
   list(
     htmltools::htmlDependency(
       "mapbox-gl",
-      version = "0.48.0",
+      version = "0.51.0",
       src = system.file("htmlwidgets/lib/mapboxgl", package = "deckgl"),
       script = c("mapbox-gl.js"),
       stylesheet = "mapbox-gl.css",
