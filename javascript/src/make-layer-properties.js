0ac359bd8e7724b229faaa6640dfee445b8a8459
skuethe <crazycapivara@gmail.com>
2019-07-22 20:53:23 +0200

    Refactor

--- javascript/src/make-layer-properties.js
+++ javascript/src/make-layer-properties.js
@@ -1,11 +1,10 @@
 import makeDataAccessor from "./helpers/make-data-accessor";
 
 export default function(el, layer) {
-  const self = {};
   const properties = layer.properties;
   const tooltipElement = _deckGLWidget._store[el.id].tooltipElement;
 
-  self._makeDataAccessors = function() {
+  const _makeDataAccessors = () => {
     for (let key of Object.keys(properties)) {
       let property = properties[key];
       if (typeof property === "object" && property.dataAccessor !== undefined) {
@@ -14,7 +13,7 @@ export default function(el, layer) {
     }
   };
 
-  self._setShinyCallback = function() {
+  const _setShinyCallback = () => {
     if (HTMLWidgets.shinyMode) {
       properties.onClick = info => {
         let data = { lng: info.lngLat[0], lat: info.lngLat[1], object: info.object };
@@ -23,7 +22,7 @@ export default function(el, layer) {
     }
   };
 
-  self._setTooltip = function() {
+  const _setTooltip = () => {
     const getTooltip = properties.getTooltip;
     const fixedTooltip = properties.fixedTooltip;
     if (getTooltip) {
@@ -44,16 +43,16 @@ export default function(el, layer) {
     }
   };
 
-  self._flipData = function() {
+  const _flipData = () => {
     if (properties.dataframeToD3) {
       layer.data = HTMLWidgets.dataframeToD3(layer.data);
     }
   };
 
-  self._makeDataAccessors();
-  self._setShinyCallback();
-  self._setTooltip();
-  self._flipData();
+  _makeDataAccessors();
+  _setShinyCallback();
+  _setTooltip();
+  _flipData();
   properties.data = layer.data;
   return properties;
 }
