4d1ff8ecc68d11fb0ba312e05fa35423b2431c6c
skuethe <crazycapivara@gmail.com>
2020-02-17 21:35:14 +0100

    Update column layer example

--- inst/examples/deckgl-api-reference/column-layer.R
+++ inst/examples/deckgl-api-reference/column-layer.R
@@ -1,14 +1,16 @@
 ## @knitr column-layer
+data("bart_stations")
+
 deck <- deckgl(zoom = 9, pitch = 35) %>%
   add_column_layer(
     data = bart_stations,
     diskResolution = 10,
     radius = 500,
-    getPosition = get_position("lat", "lng"),
-    getElevation = get_property("exits"),
+    getPosition = ~lng + lat,
+    getElevation = ~exits,
     getFillColor = JS("d => [48, 128, Math.sqrt(d.exits)]"),
     extruded = TRUE,
-    getTooltip = get_property("name")
+    getTooltip = ~name
   ) %>%
   add_mapbox_basemap()
 
