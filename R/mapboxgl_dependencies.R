37d3698362b4393f8c8e85dca8561f95b7c469c5
skuethe <crazycapivara@gmail.com>
2020-02-23 19:43:34 +0100

    Mapbox GL JS 1.8.0

--- R/mapboxgl_dependencies.R
+++ R/mapboxgl_dependencies.R
@@ -2,7 +2,7 @@ mapboxgl_dependencies <- function() {
   list(
     htmltools::htmlDependency(
       "mapbox-gl",
-      version = "0.48.0",
+      version = "1.8.0",
       src = system.file("htmlwidgets/lib/mapboxgl", package = "deckgl"),
       script = c("mapbox-gl.js"),
       stylesheet = "mapbox-gl.css",
