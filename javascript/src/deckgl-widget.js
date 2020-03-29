21b6950bd8717d0ac8fdc110df3759dd0d96e5b2
skuethe <crazycapivara@gmail.com>
2020-03-29 13:24:16 +0200

    Create deckGL instance

--- javascript/src/deckgl-widget.js
+++ javascript/src/deckgl-widget.js
@@ -1,11 +1,42 @@
+// TODO: Use 'deckGLProperties' parameter in R
+function createDeckGLProperties(elementId, widgetData) {
+   return Object.assign({
+     mapboxApiAccessToken: widgetData.mapboxApiAccessToken || "",
+     mapStyle: widgetData.mapStyle || "",
+     container: elementId,
+     initialViewState: widgetData.initialViewState || createInitialViewState(widgetData),
+     views: widgetData.views || new deck.MapView(),
+     controller: true,
+     layers: [ ]
+   }, widgetData.properties);
+}
+
+// TODO: Create initial view state already in R
+function createInitialViewState(widgetData) {
+  return {
+    longitude: widgetData.longitude,
+    latitude: widgetData.latitude,
+    zoom: widgetData.zoom,
+    pitch: widgetData.pitch,
+    bearing: widgetData.bearing
+  };
+}
+
 export default function(widgetElement, width, height) {
   const widget = { };
 
   let deckGL = null;
 
+  function _render() {
+
+  }
+
   widget.renderValue = function(widgetData) {
     console.log(widgetData);
-    widgetElement.innerHTML = "deckgl-widget";
+    // widgetElement.innerHTML = "deckgl-widget";
+
+    const deckGLProperties = createDeckGLProperties(widgetElement.id, widgetData);
+    deckGL = new deck.DeckGL(deckGLProperties);
   };
 
   widget.resize = function(width, height) {
--- javascript/src/test.js
+++ javascript/src/test.js
@@ -1,7 +1,13 @@
 import deckGLWidget from "./deckgl-widget";
 
 const WIDGET_DATA = {
-  "it": "works"
+  latitude: 37.8,
+  longitude: -122.45,
+  zoom: 12,
+  pitch: 0,
+  bearing: 0,
+  layers: [ ],
+  mapStyle: "https://basemaps.cartocdn.com/gl/dark-matter-gl-style/style.json"
 };
 
 const widgetElement = document.getElementById("widget");
