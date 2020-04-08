bacf217a5b0f39cb9e8b82e730d211d52b80557a
skuethe <crazycapivara@gmail.com>
2020-04-08 19:26:15 +0200

    Typo

--- inst/examples/deckgl-api-reference/arc-layer.R
+++ inst/examples/deckgl-api-reference/arc-layer.R
@@ -8,7 +8,7 @@ properties <- list(
   getTargetPosition = ~to_lng + to_lat,
   getSourceColor = JS("d => [Math.sqrt(d.inbound), 140, 0]"),
   getTargetColor = JS("d => [Math.sqrt(d.outbound), 140, 0]"),
-  tooltip = list(
+  tooltip = use_tooltip(
     html = "{{from_name}} to {{to_name}}",
     style = "background: steelBlue; border-radius: 5px;"
   )
@@ -16,7 +16,7 @@ properties <- list(
 
 deck <- deckgl(zoom = 10, pitch = 35) %>%
   add_arc_layer(data = bart_segments, properties = properties) %>%
-  add_control("Path Layer", "top-left") %>%
+  add_control("Arc Layer", "top-left") %>%
   add_basemap()
 
 if (interactive()) deck
