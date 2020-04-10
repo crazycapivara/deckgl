1824cb7658e03c8187a290c5045b2727e28849c9
skuethe <crazycapivara@gmail.com>
2020-04-10 14:06:10 +0200

    Update docs

--- R/tooltip.R
+++ R/tooltip.R
@@ -4,9 +4,11 @@
 #' @param ... not used
 #'
 #' @section Tooltip template Syntax:
-#' The tooltip template is a string in which variable names are identified
-#' by the double curly brackets that surround them. The variable names available to the
+#' The tooltip string is a \emph{mustache} template in which variable names are identified
+#' by the double curly brackets (\emph{mustache} tags) that surround them. The variable names available to the
 #' template are given by deck.gl’s \code{pickingInfo.object} and vary by layer.
+#'
+#' @seealso \href{https://github.com/janl/mustache.js}{mustache} for a complete syntax overwiew.
 #'
 #' @examples
 #' data("bart_segments")
@@ -14,7 +16,7 @@
 #' props <- list(
 #'   tooltip = use_tooltip(
 #'     html = "{{from_name}} to {{to_name}}",
-#'     "{{from_name}} to {{to_name}}"
+#'     style = "background: steelBlue; border-radius: 5px;"
 #'   )
 #' )
 #' @export
