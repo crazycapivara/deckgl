3de8f09da59f38764c2eadcbf187b313b0e594e8
skuethe <crazycapivara@gmail.com>
2020-04-17 14:28:04 +0200

    Update test

--- javascript/src/test-components/grid-layer.js
+++ javascript/src/test-components/grid-layer.js
@@ -34,7 +34,7 @@ const LEGEND_ARGS = {
   ],
   title: "Legend",
   pos: "top-right",
-  style: "background: yellow; width: 100px; margin-right: -10px; float: right;"
+  style: "background: yellow; width: 100px;"
 };
 
 export default {
