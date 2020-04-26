b46e6d00c39e7b6164ee98ea4e295df1cb9a92ff
skuethe <crazycapivara@gmail.com>
2020-04-26 13:45:14 +0200

    Refactor 'add_json_editor'

--- inst/examples/deckgl-api-reference/grid-layer.R
+++ inst/examples/deckgl-api-reference/grid-layer.R
@@ -7,6 +7,7 @@ properties <- list(
   cellSize = 200,
   elevationScale = 4,
   getPosition = ~lng + lat,
+  colorRange = RColorBrewer::brewer.pal(6, "YlOrRd"),
   tooltip = "{{position.0}}, {{position.1}}<br/>Count: {{count}}"
 )
 
@@ -15,6 +16,6 @@ deck <- deckgl(zoom = 11, pitch = 45, bearing = 35, element_id = "grid-layer") %
   add_grid_layer(source = "sf-bike-parking", properties = properties) %>%
   add_control("Grid Layer") %>%
   add_basemap() %>%
-  add_json_editor()
+  add_json_editor(wrap = 50)
 
 if (interactive()) deck
