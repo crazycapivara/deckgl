cad8ffb94f983b644eb84adf1eac40dd9545e20a
skuethe <crazycapivara@gmail.com>
2020-04-30 18:47:45 +0200

    Add source as dep

--- R/sources.R
+++ R/sources.R
@@ -4,19 +4,6 @@
 #' @param id The unique id of the source.
 #' @export
 add_source <- function(deckgl, id, data) {
-  #source <- create_source(data)
   if (inherits(data, "sf")) data <- modify_sf(data)
-
   invoke_method(deckgl, "addSource", id = id, data = data, convertData = inherits(data, "data.frame"))
 }
-
-#create_source <- function(data) {
-#  if (inherits(data, "sf")) {
-#    data <- modify_sf(data)
-#  }
-#
-#  list(
-#    data = data,
-#    df = inherits(data, "data.frame")
-#  )
-#}
