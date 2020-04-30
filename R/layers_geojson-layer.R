6c5cdfc9fe869f474b4d2cb31264c86f23cefb81
skuethe <crazycapivara@gmail.com>
2020-04-30 19:18:49 +0200

    Remove blank lines

--- R/layers_geojson-layer.R
+++ R/layers_geojson-layer.R
@@ -2,13 +2,9 @@
 #'
 #' The \code{GeoJsonLayer} takes in \href{http://geojson.org}{GeoJson} formatted data
 #' and renders it as interactive polygons, lines and points.
-#'
 #' @inheritParams add_layer
-#'
 #' @seealso \url{https://deck.gl/#/documentation/deckgl-api-reference/layers/geojson-layer}
-#'
 #' @example inst/examples/deckgl-api-reference/geojson-layer.R
-#'
 #' @export
 add_geojson_layer <- function(deckgl, id = "geojson-layer", data = NULL, properties = list(), ...) {
   add_layer(deckgl, "GeoJsonLayer", id, data, properties, ...)
