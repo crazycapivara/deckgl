6c4ee77af6a5c7920b3edf797852499516bb480f
skuethe <crazycapivara@gmail.com>
2020-05-10 14:07:58 +0200

    Remove dead code

--- javascript/src/layer.js
+++ javascript/src/layer.js
@@ -1,16 +1,8 @@
-// import { parse, compile } from "expression-eval";
 import { render as mustacheRender } from "mustache";
-import {
-  CLASS_NAME_TOOLTIP,
-  /*FUNCTION_IDENTIFIER*/ } from "./constants";
 import {
   convertExpressions,
   convertColors } from "./utils";
-
-// Move to utils
-//const COLOR_PROPS = [ "getColor", "getStrokeColor", "getFillColor" ];
-//const isFunction = (value) => typeof value === "string" && value.startsWith(FUNCTION_IDENTIFIER);
-//const toColor = (specifier) => typeof specifier === "string" ? convertColor(specifier) : specifier;
+import { CLASS_NAME_TOOLTIP } from "./constants";
 
 export default function(props, widgetElement) {
   // Pass data back to R in 'shinyMode'
@@ -28,44 +20,16 @@ export default function(props, widgetElement) {
   // TODO: Support deprecated? 'getTooltip' property
   const tooltip = props.tooltip || props.getTooltip;
   if (tooltip) {
-    props.onHover = toTooltip(tooltip);
+    props.onHover = convertTooltip(tooltip);
   }
 
   props = Object.assign(props, convertExpressions(props));
-  return Object.assign(props, convertColors(props));
+  props = Object.assign(props, convertColors(props));
+  return props;
 }
 
-/*
-function convertJSON(props) {
-  const convertedProps= { };
-  for (const key in props) {
-    const value = props[key];
-    if (isFunction(value)) convertedProps[key] = convertExpression(value);
-  }
-
-  return convertedProps;
-}
-*/
-
-/*
-function convertColors(props) {
-  const convertedProps = { };
-  for (const key in props) {
-    const value = props[key];
-    if (key === "colorRange") {
-      convertedProps[key] = value.map(specifier => toColor(specifier));
-    } else if (COLOR_PROPS.includes(key)) {
-      convertedProps[key] = (data) => {
-        const specifier = typeof value === "function" ? value(data) : value;
-        return toColor(specifier);
-      };
-    }
-  }
-
-  return convertedProps;
-}
-*/
-function toTooltip(tooltip) {
+// TODO: Maybe also move to 'utils'
+function convertTooltip(tooltip) {
   const tooltipElement = document.getElementsByClassName(CLASS_NAME_TOOLTIP)[0];
   if (tooltip.style) tooltipElement.style.cssText = tooltip.style;
   return function({ x, y, object }) {
@@ -77,16 +41,9 @@ function toTooltip(tooltip) {
 
     tooltipElement.style.top = y + "px";
     tooltipElement.style.left = x + "px";
-    tooltipElement.innerHTML = typeof tooltip === "function" ?
-      tooltip(object) : mustacheRender(typeof tooltip === "string" ? tooltip : tooltip.html, object);
+    tooltipElement.innerHTML = typeof tooltip === "function"
+      ? tooltip(object)
+      : mustacheRender(typeof tooltip === "string" ? tooltip : tooltip.html, object);
     tooltipElement.style.display = "block";
   };
 }
-
-/*
-function toFunction(value) {
-  const expr = value.replace(FUNCTION_IDENTIFIER, "");
-  const func = compile(expr);
-  return (data) => func(Object.assign({ "Math": Math, "console": console }, data));
-}
-*/
