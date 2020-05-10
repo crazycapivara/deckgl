8e4d9c178c048ca8e7faf783ff5df5ec75d1501a
skuethe <crazycapivara@gmail.com>
2020-05-10 11:17:57 +0200

    Use expressions when converting formulas

--- R/formula-to-property.R
+++ R/formula-to-property.R
@@ -1,13 +1,18 @@
+EXPRESSION_IDENTIFIER <- "@="
+
+# TODO: Rename to 'convert_formulas'
 formula_to_property <- function(properties) {
   for (key in names(properties)) {
     value <- properties[[key]]
     if (inherits(value, "formula")) {
       x <- all.vars(value)
       if (length(x) > 1) {
-        properties[[key]] <- sprintf("data => [%s]", paste0("data.", x, collapse = ", ")) %>%
-          JS()
+        properties[[key]] <- paste0(x, collapse = ", ") %>%
+          paste0(EXPRESSION_IDENTIFIER,"[", ., "]")
+        #properties[[key]] <- sprintf("data => [%s]", paste0("data.", x, collapse = ", ")) %>%
+        #  JS()
       } else {
-        properties[[key]] <- get_property(x[1])
+        properties[[key]] <- paste0(EXPRESSION_IDENTIFIER, x[1]) #get_property(x[1])
       }
     }
   }
