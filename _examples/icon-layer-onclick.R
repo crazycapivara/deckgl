b818fbc4075b36ff4628d622757cae84a1c25966
fry <fry@futurama.de>
2022-07-09 12:12:04 +0200

    Update onclick example

--- _examples/icon-layer-onclick.R
+++ _examples/icon-layer-onclick.R
@@ -1,4 +1,17 @@
+library(deckgl)
+
 sample_data <- "https://raw.githubusercontent.com/uber-common/deck.gl-data/master/website/bart-stations.json"
+
+callback <- '
+  function({ x, y, object }) {
+    tooltip = document.getElementsByClassName("deckgl-widget-tooltip")[0];
+    tooltip.style.display = "block";
+    tooltip.innerHTML = object.name +  "</br>" + object.address;
+    /* uncomment if you want to move the tooltip */
+    // tooltip.style.top = y + "px";
+    // tooltip.style.left = x + "px";
+  }
+'
 
 deckgl() %>%
   # Use default icon
@@ -8,6 +21,6 @@ deckgl() %>%
     getColor = JS("d => [Math.sqrt(d.exits), 140, 0]"), # overwrite default color
     pickable = TRUE,
     # onClick = JS("info => console.log(info)")
-    onClick = JS("info => deckglWidget.tooltipElement.innerHTML = info.object.name")
+    onClick = JS(callback)
   ) %>%
   add_basemap()
