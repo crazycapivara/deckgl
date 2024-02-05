b325faca808972142b1693544442f8081e2038a0
Salim Damerdji <31995532+sdamerdji@users.noreply.github.com>
2024-02-05 07:20:36 -0800

    avoid get_data deprecation

--- _examples/multiple-layers.R
+++ _examples/multiple-layers.R
@@ -5,18 +5,19 @@ bart_stations <- system.file("sample-data/bart-stations.json", package = "deckgl
 main_properties <- list(
   getPosition = get_position("lat", "lng")
 )
+SOURCE_ID = 'bart-stations'
 
 deckgl(zoom = 10, pitch = 35) %>%
-  add_data(bart_stations) %>%
+  add_source(SOURCE_ID, data=bart_stations) %>%
   add_scatterplot_layer(
-    data = get_data(),
+    source=SOURCE_ID,
     properties = main_properties,
     radiusScale = 6,
     getRadius = 50,
     getColor = c(240, 140, 20)
   ) %>%
   add_text_layer(
-    data = get_data(),
+    source=SOURCE_ID,
     properties = main_properties,
     getText = get_property("name"),
     getAlignmentBaseline = "bottom"
